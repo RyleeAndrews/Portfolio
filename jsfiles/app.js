18c5ef4947ae6d46a48d8f914a6b72044cb63806
RyleeAndrews <ryleeandrews73@gmail.com>
2017-05-12 14:44:18 -0700

    almost ready for resubmission

--- jsfiles/app.js
+++ jsfiles/app.js
@@ -2,18 +2,26 @@
 var articles = [];
 function Article (rawDataObj){
   this.title = rawDataObj.title;
-  this.name = rawDataObj.name;
   this.githubUrl = rawDataObj.githubUrl;
+  this.author = rawDataObj.author;
   this.publishedOn = rawDataObj.publishedOn;
   this.body = rawDataObj.body;
 }
 
 
 $(document).ready(function () {
-  $('ul li:first-child').remove()
+  $('ul li').eq(1).remove();
 })
-
 
 $(document).ready(function() {
   $('body').css('background-color', 'beige');
 })
+$(document).ready(function () {
+  $('#main-nav').hide();
+})
+$('#icon').on('click', function () {
+  $('#main-nav').show();
+})
+Article.prototype.toHtml = function (){
+  var $newArticle = $('new-article').clone();
+};
